fae0918b53eb6c04b1677fd81d63e6bdba6374df
Belgin Android <61349423+Belgin-Android@users.noreply.github.com>
2020-05-28 17:39:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Icons from "react-native-vector-icons/Ionicons"
+import {createAppContainer,createSwitchNavigator} from "react-navigation";
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import {createStackNavigator} from "react-navigation-stack";
+import ForgotScreen from "./screens/ForgotScreen";
+import SearchScreen from "./screens/SearchScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import MessageScreen from "./screens/MessageScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAT6YNS8H1NQQajHhO0rmj6tW_n9E1n-ZE",
+  authDomain: "belginterno.firebaseapp.com",
+  databaseURL: "https://belginterno.firebaseio.com",
+  projectId: "belginterno",
+  storageBucket: "belginterno.appspot.com",
+  messagingSenderId: "263916040774",
+  appId: "1:263916040774:web:9a94f60c7437d9a4e15047",
+  measurementId: "G-3D12ELXXKL"
+};
+// Initialize Firebase
+if (!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+
+const   AppTabNavigator = createBottomTabNavigator({
+
+  Profile:{
+    screen:ProfileScreen,
+    navigationOptions:{
+      tabBarIcon: ({ tintColor }) => (<Icon name="account" size={25} color={tintColor}/>),
+    }
+  },
+  
+  Message:{
+    screen:MessageScreen,
+    navigationOptions:{
+      tabBarIcon:({ tintColor}) => (<Icons name="ios-send" size={25} color={tintColor} />),
+    }
+  },
+  Search:{
+    screen:SearchScreen,
+    navigationOptions:{
+      tabBarIcon:({ tintColor}) => (<Icons name="md-search" size={25} color={tintColor} />),
+    }
+  },
+  Notification:{
+    screen:NotificationScreen,
+    navigationOptions:{
+      tabBarIcon: ({ tintColor }) => (<Icon name="firebase" size={25} color={tintColor} style={{}} />),
+      
+    }
+  },
+ 
+},
+{
+  tabBarOptions:{
+    activeTintColor: '#448AFF',
+    inactiveTintColor:"#514E5A",
+    showLabel:true,
+    style:{height:55},
+    tabBarPosition:"bottom",
+    swipeEnabled:true,
+  }
+}
+)
+;
+
+const AuthStack = createStackNavigator({
+  Login:LoginScreen,
+  Register:RegisterScreen,
+  Forgot:ForgotScreen,
+  
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading:LoadingScreen,
+      App:AppTabNavigator,
+      Auth:AuthStack
+    },
+    {
+      initialRouteName:"Loading",
+    }
+  )
+);
